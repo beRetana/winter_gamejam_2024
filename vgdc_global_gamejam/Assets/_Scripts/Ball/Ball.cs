7cbcc40d50c0cb42e070198bd1b99728df0dc8ab
Brandon Eduardo Retana García <142031627+beRetana@users.noreply.github.com>
2025-01-25 10:15:40 -0800

    ball updates

--- vgdc_global_gamejam/Assets/_Scripts/Ball/Ball.cs
+++ vgdc_global_gamejam/Assets/_Scripts/Ball/Ball.cs
@@ -1,8 +1,25 @@
 using System;
+using Unity.Mathematics;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Ball : MonoBehaviour
 {
+    [SerializeField] private float _rotationSpeed = 0.01f;
+
+    private Quaternion _targetRotation = Quaternion.Euler(0, 0, 360);
+    
+    void Update()
+    {
+        if (_targetRotation == Quaternion.identity)
+        {
+            _targetRotation = Quaternion.Euler(0, 0, 360);
+        }
+
+        transform.rotation = Quaternion.Lerp(transform.rotation, _targetRotation, Time.time * _rotationSpeed);
+        Debug.Log("Ball Rotation: " + transform.rotation);
+    }
+
     void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.TryGetComponent(out IPeg peg))
