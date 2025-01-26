40dd42a33d8db1bc89b6bad8f1a2b2d4e50da6e8
ctnp-e <catnipiscatweed@gmail.com>
2025-01-26 00:23:11 -0800

    right click = bottom goes faster

--- vgdc_global_gamejam/Assets/_Scripts/Controllers/BallBackHole/BallBackHoleMovement.cs
+++ vgdc_global_gamejam/Assets/_Scripts/Controllers/BallBackHole/BallBackHoleMovement.cs
@@ -1,15 +1,36 @@
 using UnityEngine;
+using System.Collections;
 
 public class BallBackHoleMovement : MonoBehaviour
 {
     private const string WALL_TAG = "Wall";
 
     [SerializeField] private float speed;
+    private float speedMultiplier = 4f;
+
+    private float og_speed;
+
+
 
     private int direction = 1;
-    
+    private  void Awake()
+    {
+        og_speed = speed;
+    }
+
     private void Update()
     {
+        
+        if (Input.GetMouseButton(1) && 
+            DataMessenger.GetBool(BoolKey.IsBallInPlay) == false) 
+        // if you hold down right click speed increases
+        {
+            speed = og_speed * speedMultiplier;
+        }
+        else
+        {
+            speed = og_speed;
+        }
         transform.position += speed * Time.deltaTime * direction * Vector3.right;
     }
 
@@ -20,4 +41,5 @@ public class BallBackHoleMovement : MonoBehaviour
             direction = -direction;
         }
     }
+
 }
